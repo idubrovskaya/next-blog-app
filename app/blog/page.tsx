28c64fb283c9a5a166d434aa0448f90520588c79
Irina Dubrovskaya <irina.dubrovskaya95@gmail.com>
2023-11-01 11:32:03 +0300

    search using zustand

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,4 @@
-'use client';
-
-import { getAllPosts } from '@/services/getPosts';
 import { Metadata } from 'next';
-import { useEffect, useState } from 'react';
 import { Posts } from '../components/Posts';
 import { PostSearch } from '../components/PostsSearch';
 
@@ -11,20 +7,11 @@ export const metadata: Metadata = {
 };
 
 export default function Blog() {
-  const [posts, setPosts] = useState<any[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    getAllPosts()
-      .then(setPosts)
-      .finally(() => setLoading(false));
-  }, []);
-
   return (
     <>
       <h1>Blog</h1>
-      <PostSearch onSearch={setPosts} />
-      {loading ? <h3>Loading...</h3> : <Posts posts={posts} />}
+      <PostSearch />
+      <Posts />
     </>
   );
 }
